bc9fb36648c6c7c04d261cab8da4e25095355f98
Iamneo <techadmin@iamneo.ai>
2023-11-09 12:00:04 +0000

    Backup Commit

---
+++ angularapp/src/app/first.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'first-component',
+    templateUrl:'./first.component.html',
+})
+
+export class FirstComponent
+{
+    greet:string="Hello";
+}

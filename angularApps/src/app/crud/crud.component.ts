de13d0a18957a0ce1b2082322ec8b7a6640ec7d8
Iamneo <techadmin@iamneo.ai>
2023-11-11 08:08:05 +0000

    Backup Commit

--- angularApps/src/app/crud/crud.component.ts
+++ angularApps/src/app/crud/crud.component.ts
@@ -1,6 +1,6 @@
-// import { stringify } from "querystring";
-// import { ThrowStmt } from "@angular/compiler";
+import { ThrowStmt } from "@angular/compiler";
 import { Component } from "@angular/core";
+import { timeStamp } from "console";
 
 
 @Component({
@@ -10,133 +10,166 @@ import { Component } from "@angular/core";
 export class Crud
 {
     indiData:object = {email:" ",password:" ",isReady:false};
-    userData:object[]=[ {email:" ",password:" ",isReady:false}];
+    email:string = " "
+    password:string = ""
+    isReady:boolean = false;
+
+    userData:object[]=[{}];
+
+    
 
     dataHandler(event){
+   let id = event.target.id;
 
-        let id = event.target.id;
-        if(id == "btn1"){
+   if(id == "btn1"){
+       console.log(this.email);
+       console.log(this.password);
+       console.log(this.isReady);
+       this.userData.push({email:this.email,password:this.password,isReady:this.isReady})
+       console.log(this.userData)
+   }
+   else if(id=="upbtn"){
 
-            console.log(this.indiData);
-            this.userData.push(this.indiData);
-        }
-        else if(id == "upbtn")
-        {
+    
 
-        }
-        else if(id == "emId")
-        {
 
-        }
+   }
+   else if(id=="emId"){
+      if(this.userData.length > 1){
+       let data= this.userData.find(x=>x.email == this.email);
+       this.password = data.password;
+       this.isReady= data.isReady;
+      }
+   }
+       
+
     //    let ids:string = event.target.id;
     //    let data:any = event.target.value;
 
+      // console.log(this.indiData.email);
+
+       
         // switch (ids) {
-        //        case "emId":
-        //             //  this.indiData.email = data;
-        //                 this.indiData = {...this.indiData,email:data};  
-        //                 if(this.indiData.length != 0)
-        //                 {
-        //                     console.log("Working.....");
-        //                     let value = this.userData.find(x=> x.email == this.indiData.email);
-        //                     this.indiData = (...this.indiData,password:this.indiData.password,isReady:this.indiData.isReady)
-        //                 }     
-                
-        //             // this.indiData = {...this.indiData,email:data};
-        //             // if(this.userData.length != 0){
-        //             // let value =this.userData.find(x=>x.email == this.indiData.email);
-        //             // }
-        //              break;
+        //     case "emId":
+        //      this.indiData = {...this.indiData,email:data};
+        //      if(this.userData.length != 0){
+        //         console.log("working..")
+        //         let value = this.userData.find(x=>x.email == this.indiData.email);
+
+        //          this.indiData = {...value,password:value.password,isReady:value.isReady};
+
+        //         console.log(this.indiData);
+        //      }
+        //         break;
         //         case "psId":
-        //             this.indiData = {...this.indiData,password:data};
-        //             break;
+        //             this.indiData={...this.indiData,password: data}
+        //         break;
         //         case "chId":
-        //                 // this.indiData.isReady = data;
-        //                 let chkVal = event.target.checked;
-        //                 this.indiData = {...this.indiData,isReady:chkVal};
-        //             break;
+        //            let chkVal = event.target.checked;
+        //             this.indiData={...this.indiData,isReady: chkVal}
+        //         break;
                
         //         case "btn1":
-        //         //  console.log(this.indiData)
+              
         //          this.userData.push(this.indiData);
-        //          console.log(this.userData);
-        //              break;
-
-        //         case "upbtn":
-        
-        //             this.userData.forEach((value,indx,arr)=>{
-        //                 if(value.email == this.indiData.email){
-        //                     console.log(value);
-                            
-        //                 }
-        //             }); 
-        //         //console.log(value);
-        //         this.userData.push(this.indiData);
+               
+        //         break;
+        //         case "upbtn":              
+               
+                 
                 
+                
+                
+               
         //         break;
         
         //     default:
         //         break;
         // }
-        console.log(event.target.id);
+        // console.log(event.target.id);
    }
 }
-/*
-import { ThrowStmt } from "@angular/compiler";
-import { Component } from "@angular/core";
+
+// // import { stringify } from "querystring";
+// // import { ThrowStmt } from "@angular/compiler";
+// import { Component } from "@angular/core";
 
 
-@Component({
-    selector:'crud',
-    templateUrl:'./crud.component.html'
-})
-export class Crud
-{
-    indiData:object = {email:" ",password:" ",isReady:false};
-    userData:object[]=[{email:" ",password:" ",isReady:false}];
+// @Component({
+//     selector:'crud',
+//     templateUrl:'./crud.component.html'
+// })
+// export class Crud
+// {
+//     indiData:object = {email:" ",password:" ",isReady:false};
+//     userData:object[]=[ {email:" ",password:" ",isReady:false}];
 
-    dataHandler(event){
-       let ids:string = event.target.id;
-       let data:any = event.target.value;
-       
-        switch (ids) {
-            case "emId":
-             this.indiData = {...this.indiData,email:data};
-             if(this.userData.length != 0){
-             let value =this.userData.find(x=>x.email == this.indiData.email);
+//     dataHandler(event){
 
-            
-             }
-                break;
-                case "psId":
-                    this.indiData={...this.indiData,password: data}
-                break;
-                case "chId":
-                   let chkVal = event.target.checked;
-                    this.indiData={...this.indiData,isReady: chkVal}
-                break;
+//         let id = event.target.id;
+//         if(id == "btn1"){
+
+//             console.log(this.indiData);
+//             this.userData.push(this.indiData);
+//         }
+//         else if(id == "upbtn")
+//         {
+
+//         }
+//         else if(id == "emId")
+//         {
+
+//         }
+//     //    let ids:string = event.target.id;
+//     //    let data:any = event.target.value;
+
+//         // switch (ids) {
+//         //        case "emId":
+//         //             //  this.indiData.email = data;
+//         //                 this.indiData = {...this.indiData,email:data};  
+//         //                 if(this.indiData.length != 0)
+//         //                 {
+//         //                     console.log("Working.....");
+//         //                     let value = this.userData.find(x=> x.email == this.indiData.email);
+//         //                     this.indiData = (...this.indiData,password:this.indiData.password,isReady:this.indiData.isReady)
+//         //                 }     
+                
+//         //             // this.indiData = {...this.indiData,email:data};
+//         //             // if(this.userData.length != 0){
+//         //             // let value =this.userData.find(x=>x.email == this.indiData.email);
+//         //             // }
+//         //              break;
+//         //         case "psId":
+//         //             this.indiData = {...this.indiData,password:data};
+//         //             break;
+//         //         case "chId":
+//         //                 // this.indiData.isReady = data;
+//         //                 let chkVal = event.target.checked;
+//         //                 this.indiData = {...this.indiData,isReady:chkVal};
+//         //             break;
                
-                case "btn1":
-                 
-                 this.userData.push(this.indiData);
-               
-                break;
-                case "upbtn":
-                 
-                this.userData.forEach((value,indx,arr)=>{
-                  if(value.email == this.indiData.email){
-                    
-                  }
-                }); 
-                 //console.log(value);
-                 this.userData.push(this.indiData);
-               
-                break;
+//         //         case "btn1":
+//         //         //  console.log(this.indiData)
+//         //          this.userData.push(this.indiData);
+//         //          console.log(this.userData);
+//         //              break;
+
+//         //         case "upbtn":
         
-            default:
-                break;
-        }
-        console.log(event.target.id);
-1    }
-}
-*/ 
+//         //             this.userData.forEach((value,indx,arr)=>{
+//         //                 if(value.email == this.indiData.email){
+//         //                     console.log(value);
+                            
+//         //                 }
+//         //             }); 
+//         //         //console.log(value);
+//         //         this.userData.push(this.indiData);
+                
+//         //         break;
+        
+//         //     default:
+//         //         break;
+//         // }
+//         console.log(event.target.id);
+//    }
+// }

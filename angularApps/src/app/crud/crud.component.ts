8b3dcb7ea95f016d42519c92355c704acfd3ba02
Iamneo <techadmin@iamneo.ai>
2023-11-11 05:23:06 +0000

    Backup Commit

--- angularApps/src/app/crud/crud.component.ts
+++ angularApps/src/app/crud/crud.component.ts
@@ -17,13 +17,18 @@ export class Crud
        let data:any = event.target.value;
 
         switch (ids) {
-                  case "emId":
-            //  this.indiData.email = data;
-                this.indiData = {...this.indiData,email:data};  
-                if(this.indiData.length != 0)
-                {
-                    let value = this.userData.find(x=> x.email == this.indiData.email);
-                }              
+               case "emId":
+                    //  this.indiData.email = data;
+                        // this.indiData = {...this.indiData,email:data};  
+                        // if(this.indiData.length != 0)
+                        // {
+                        //     let value = this.userData.find(x=> x.email == this.indiData.email);
+                        // }     
+                
+                    this.indiData = {...this.indiData,email:data};
+                    // if(this.userData.length != 0){
+                    // let value =this.userData.find(x=>x.email == this.indiData.email);
+                    // }
                      break;
                 case "psId":
                     this.indiData = {...this.indiData,password:data};
@@ -41,12 +46,17 @@ export class Crud
                      break;
 
                 case "upbtn":
-                  let value = this.userData.find(x=> x.email == this.indiData.email);
-                //   console.log(value);
-                  
-                    // this.userData.push(this.indiData);
-                    break;
-
+        
+                    this.userData.forEach((value,indx,arr)=>{
+                        if(value.email == this.indiData.email){
+                            console.log(value);
+                            
+                        }
+                    }); 
+                //console.log(value);
+                this.userData.push(this.indiData);
+                
+                break;
         
             default:
                 break;
@@ -54,3 +64,62 @@ export class Crud
         console.log(event.target.id);
    }
 }
+/*
+import { ThrowStmt } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'crud',
+    templateUrl:'./crud.component.html'
+})
+export class Crud
+{
+    indiData:object = {email:" ",password:" ",isReady:false};
+    userData:object[]=[{email:" ",password:" ",isReady:false}];
+
+    dataHandler(event){
+       let ids:string = event.target.id;
+       let data:any = event.target.value;
+       
+        switch (ids) {
+            case "emId":
+             this.indiData = {...this.indiData,email:data};
+             if(this.userData.length != 0){
+             let value =this.userData.find(x=>x.email == this.indiData.email);
+
+            
+             }
+                break;
+                case "psId":
+                    this.indiData={...this.indiData,password: data}
+                break;
+                case "chId":
+                   let chkVal = event.target.checked;
+                    this.indiData={...this.indiData,isReady: chkVal}
+                break;
+               
+                case "btn1":
+                 
+                 this.userData.push(this.indiData);
+               
+                break;
+                case "upbtn":
+                 
+                this.userData.forEach((value,indx,arr)=>{
+                  if(value.email == this.indiData.email){
+                    
+                  }
+                }); 
+                 //console.log(value);
+                 this.userData.push(this.indiData);
+               
+                break;
+        
+            default:
+                break;
+        }
+        console.log(event.target.id);
+1    }
+}
+*/ 

355a855c1e871cd8bfefd3d6c75c9e2f5fd26e9f
Iamneo <techadmin@iamneo.ai>
2023-11-13 08:54:35 +0000

    Backup Commit

--- angularApps/src/app/TemplateForm/demo.templateform.ts
+++ angularApps/src/app/TemplateForm/demo.templateform.ts
@@ -1,9 +1,15 @@
 import {Component} from "@angular/core";
-
+import { NgForm } from "@angular/forms";
 @Component({
     selector:'templateForm',
     templateUrl:'./demo.templateform.html'
 })
 export class TemplateForm{
 
+    submitData(form:NgForm)
+    {
+        console.log(form.value);
+        console.log(form.valid);
+        
+    }
 }

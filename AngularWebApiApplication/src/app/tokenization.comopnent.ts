21a57b7441e7749445b3ff4d2b5795519a8c0d3d
Iamneo <techadmin@iamneo.ai>
2023-11-16 05:46:17 +0000

    Backup Commit

---
+++ AngularWebApiApplication/src/app/tokenization.comopnent.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+HttpClient
+@Component({
+	selector:'toekn-demo',
+	template:`<h1> Token Demo<h1>`
+})
+
+export class TokenDemo(
+
+    constructor(private http:HttpClient){
+	
+}
+)
+Interface Idata{
+	user:string
+	password:string
+}

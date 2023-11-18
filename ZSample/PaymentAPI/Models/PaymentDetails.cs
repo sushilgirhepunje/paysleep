87fe84e67165f17fd2d2e5586e869c552f6f735e
Iamneo <techadmin@iamneo.ai>
2023-11-18 06:07:56 +0000

    Backup Commit

--- ZSample/PaymentAPI/Models/PaymentDetails.cs
+++ ZSample/PaymentAPI/Models/PaymentDetails.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace PaymentAPI.Models
 {
     public class PaymentDetails

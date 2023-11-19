3477062b697f821cf43b21899b6460e6706566ce
Iamneo <techadmin@iamneo.ai>
2023-11-19 13:31:12 +0000

    Backup Commit

--- Project/angularapp/src/app/app.module.ts
+++ Project/angularapp/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
-
+import {MatDatepickerModule} from '@angular/material/datepicker'
 import { EmployeeService } from './services/employee.service';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import {MatButtonModule,MatMenuModule} from "@angular/material";
+import {MatButtonModule} from "@angular/material/button";
 import {MatRadioModule} from "@angular/material/radio";
 
 @NgModule({
@@ -18,9 +18,13 @@ import {MatRadioModule} from "@angular/material/radio";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRadioModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [EmployeeService,HttpClientModule,MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

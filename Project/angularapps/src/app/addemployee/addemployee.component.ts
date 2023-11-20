24fe068ff1d58e52dda84774a2499f050d36575e
Iamneo <techadmin@iamneo.ai>
2023-11-20 06:49:59 +0000

    Backup Commit

--- Project/angularapps/src/app/addemployee/addemployee.component.ts
+++ Project/angularapps/src/app/addemployee/addemployee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee';
+import { EmployeeService } from '../service/employee.service';
+import { Route } from '@angular/router';
 
 @Component({
   selector: 'app-addemployee',
@@ -6,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addemployee.component.css']
 })
 export class AddemployeeComponent implements OnInit {
-empdata:any[]
-  constructor() { }
-
+  empdata:Employee = {EmpId:0,EmpName:'',DateOfBirth:"",EmailId:'',Gender:'',Address:'',PinCode:''}
+  constructor(private ms:EmployeeService,private route:Route) { }
+  saveData(employee):void{
+    this.empdata = employee;
+    this.ms.addEmployee(this.empdata).subscribe(
+      ()=>{
+        alert('Record Added Successfully')
+        this.route.navigate(['/InsertEmployeeDetails'])
+      }
+    )
+  }
   ngOnInit() {
   }
 
